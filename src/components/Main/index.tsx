6216f1686ce161a1855cea9f783e0b2737e82a01
jkevinx23 <kevingomesr@gmail.com>
2020-08-25 09:26:43 -0300

    Config storybook new version

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e o texto React Avançado escrito ao lado"
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Desctiption>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </S.Desctiption>
+    <S.Title>{title}</S.Title>
+    <S.Desctiption>{description}</S.Desctiption>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com um código."
