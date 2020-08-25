1a52a24ca84bd966ac71fb30449f5a93250e90ee
jkevinx23 <kevingomesr@gmail.com>
2020-08-25 08:47:40 -0300

    Config storybook global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+// }
+
+import { addDecorator } from '@storybook/react'
+import withGlobalStyles from './withGlobalStyles'
+
+addDecorator(withGlobalStyles);
